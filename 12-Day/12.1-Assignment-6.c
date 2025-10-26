2fb5719ad026f57a94ff2806ced03c0ca463bd3b
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2025-10-26 09:20:26 +0630

    added user edit

--- 12-Day/12.1-Assignment-6.c
+++ 12-Day/12.1-Assignment-6.c
@@ -33,6 +33,13 @@ void user_dashboard(int login_user_index);
 
 void admin_dashboard(int login_user_index);
 
+void show_user_info(int login_user_index);
+
+int confirm_password(int login_user_index);
+
+void edit_user_info(int login_user_index);
+
+
 /**
  * Functions End ...............................
  */
@@ -84,6 +91,7 @@ typedef struct {
     char password[20];
     int postcode;
     char address[50];
+    char status[10];
     int point;
     char role[10];
     POINT_TRANSACTION point_transaction[100];
@@ -98,6 +106,8 @@ DB users[10];
 const int G_USER_INITIAL_POINT = 100;
 const char G_USER_ROLE_USER[5] = "user";
 const char G_USER_ROLE_ADMIN[6] = "admin";
+const char G_STATUS_ACTIVE[7] = "active";
+const char G_STATUS_INACTIVE[10] = "inactive";
 
 int g_user_count = 0;
 int g_login_user_id = 0;
@@ -118,7 +128,6 @@ int main() {
 * Functions Start .............................
 */
 void menu() {
-
     int option = -1;
     printf("\nEnter 1 to Register\nEnter 2 to Login\nEnter 3 To Exit\n");
     printf("Enter Here => ");
@@ -168,7 +177,8 @@ void registration() {
     }
 
     while (!is_valid_password) {
-        printf("Password Must Include at least One Small Letter\nOne Capital Letter\nOne Special Character and\nMust Be at least 6 Characters Long\nEnter User Password => ");
+        printf(
+            "Password Must Include at least One Small Letter\nOne Capital Letter\nOne Special Character and\nMust Be at least 6 Characters Long\nEnter User Password => ");
         scanf(" %[^\n]", &r_password[0]);
         is_valid_password = validate_password(r_password);
     }
@@ -188,10 +198,10 @@ void registration() {
     copy_two_char_array(users[g_user_count].address, r_address);
     copy_two_char_array(users[g_user_count].role, G_USER_ROLE_USER);
     users[g_user_count].point = G_USER_INITIAL_POINT;
+    copy_two_char_array(users[g_user_count].status, G_STATUS_ACTIVE);
     g_user_count++;
 
-   printf("*********Registered Successfully*********\n");
-
+    printf("*********Registered Successfully*********\n");
 };
 
 void login() {
@@ -201,15 +211,14 @@ void login() {
     int is_success = 0;
 
 
-
     printf("Enter Your Email => ");
-    scanf(" %[^\n]",&l_email[0]);
+    scanf(" %[^\n]", &l_email[0]);
 
     printf("Enter Your Password => ");
-    scanf(" %[^\n]",&l_password[0]);
+    scanf(" %[^\n]", &l_password[0]);
 
     for (int x = 0; x < g_user_count; x++) {
-         int is_email_exist = check_two_char_array(users[x].email,l_email);
+        int is_email_exist = check_two_char_array(users[x].email, l_email);
         if (is_email_exist) {
             int is_password_exist = check_two_char_array(users[x].password, l_password);
             if (is_password_exist) {
@@ -218,45 +227,56 @@ void login() {
                 break;
             }
         }
-
     }
 
     if (is_success) {
         show_dashboard();
-    }else {
+    } else {
         printf("Login Failed!\n");
         menu();
     }
-
-
-
 }
 
 void save_data() {
     printf("*********Start Saving Data*********\n");
-    FILE *fptr = fopen("database.bin","wb");
+    FILE *fptr = fopen("database.bin", "wb");
 
     if (!fptr) {
         perror("Save Data Error\n");
     }
 
-    fwrite(users,sizeof(DB),g_user_count,fptr);
+    // char a_name[20] = "PhyoAungNaingTun";
+    // char a_email[20] = "phyoaung@gmail.com";
+    // char a_password[20] = "admin@123";
+    // int a_phone = 111111;
+    // char a_address[20] = "Hlaing";
+    //
+    //
+    // users[g_user_count].id = g_user_count + 1;
+    // copy_two_char_array(users[g_user_count].name, a_name);
+    // copy_two_char_array(users[g_user_count].email, a_email);
+    // copy_two_char_array(users[g_user_count].password, a_password);
+    // users[g_user_count].phone = a_phone;
+    // copy_two_char_array(users[g_user_count].address, a_address);
+    // copy_two_char_array(users[g_user_count].role, G_USER_ROLE_ADMIN);
+    // copy_two_char_array(users[g_user_count].status, G_STATUS_ACTIVE);
+    // g_user_count++;
+
+    fwrite(users, sizeof(DB), g_user_count, fptr);
 
     fclose(fptr);
     printf("*********Saved Data Successfully*********\n");
-
-
 }
 
 void sync_data() {
     printf("*********Start Sync Data Form*********\n");
 
-    FILE *fptr = fopen("database.bin","rb");
+    FILE *fptr = fopen("database.bin", "rb");
     if (!fptr) {
         perror("Save Data Error\n");
     }
     int index = 0;
-    while ( fread(&users[index],sizeof(DB),1,fptr) == 1) {
+    while (fread(&users[index], sizeof(DB), 1, fptr) == 1) {
         printf("\nUser Id is => %d\n", users[index].id);
         printf("User Name is => %s\n", users[index].name);
         printf("User Email is => %s\n", users[index].email);
@@ -264,21 +284,21 @@ void sync_data() {
         printf("User Postcode is => %d\n", users[index].postcode);
         printf("User Point is => %d\n", users[index].point);
         printf("User Address is => %s\n", users[index].address);
+        printf("User Status is => %s\n", users[index].status);
         printf("User Role is => %s\n", users[index].role);
         index++;
     }
     g_user_count = index;
     fclose(fptr);
-    printf("\n*********Finish Sync Data Form*********\n");
 
+    printf("\n*********Finish Sync Data Form*********\n");
 }
 
 int validate_phone(int phone) {
-
     int is_phone_duplicate = check_phone_duplication(phone);
 
     if (is_phone_duplicate) {
-        printf("\nPhone Number (%d) has been used!\n",phone);
+        printf("\nPhone Number (%d) has been used!\n", phone);
         return 0;
     }
     return 1;
@@ -316,9 +336,9 @@ int validate_email(char email[50]) {
         }
     }
 
-    char gmail_symbol[11] = {'@','g','m','a','i','l','.','c','o','m'};
-    char apple_mail_symbol[11] = {'@','a','p','p','l','e','.','c','o','m'};
-    char yahoo_mail_symbol[11] = {'@','y','a','h','o','o','.','c','o','m'};
+    char gmail_symbol[11] = {'@', 'g', 'm', 'a', 'i', 'l', '.', 'c', 'o', 'm'};
+    char apple_mail_symbol[11] = {'@', 'a', 'p', 'p', 'l', 'e', '.', 'c', 'o', 'm'};
+    char yahoo_mail_symbol[11] = {'@', 'y', 'a', 'h', 'o', 'o', '.', 'c', 'o', 'm'};
     char second_part[20];
     int index = 0;
 
@@ -331,8 +351,8 @@ int validate_email(char email[50]) {
     }
 
     int is_valid_gmail_symbol = check_two_char_array(gmail_symbol, second_part);
-    int is_valid_yahoo_mail_symbol = check_two_char_array(yahoo_mail_symbol,second_part);
-    int is_valid_apple_mail_symbol = check_two_char_array(apple_mail_symbol,second_part);
+    int is_valid_yahoo_mail_symbol = check_two_char_array(yahoo_mail_symbol, second_part);
+    int is_valid_apple_mail_symbol = check_two_char_array(apple_mail_symbol, second_part);
 
     if (!(is_valid_gmail_symbol || is_valid_yahoo_mail_symbol || is_valid_apple_mail_symbol)) {
         printf("\nEmail (%s) is invalid!\n", email);
@@ -363,11 +383,11 @@ int validate_password(char password[20]) {
     for (int x = 0; x < size_of_password; x++) {
         if (is_number(password[x])) {
             is_include_number = 1;
-        }else if (is_small_letter(password[x])) {
+        } else if (is_small_letter(password[x])) {
             is_include_small_letter = 1;
-        }else if (is_capital_letter(password[x])) {
+        } else if (is_capital_letter(password[x])) {
             is_include_capital_letter = 1;
-        }else if (is_special_character(password[x])) {
+        } else if (is_special_character(password[x])) {
             is_include_special_character = 1;
         }
     }
@@ -384,22 +404,174 @@ int validate_password(char password[20]) {
 void show_dashboard() {
     printf("\n****Welcome To IDS Delivery ****\n");
     int user_index = get_auth_user_index();
-    if (check_two_char_array(G_USER_ROLE_USER,users[user_index].role)) {
+    if (check_two_char_array(G_USER_ROLE_USER, users[user_index].role)) {
         user_dashboard(user_index);
-    }else {
+    } else {
         admin_dashboard(user_index);
     }
-
 }
 
 void user_dashboard(int login_user_index) {
-
+    printf("\n Welcome User (%s) \n", users[login_user_index].name);
+    int option = 0;
+    printf("Enter 1 To See Your Info. Enter 2 To Edit Your Info.\nEnter 3 To Transfer Points. Enter 4 To Logout\n");
+    printf("Enter Here => ");
+    scanf("%d", &option);
+    switch (option) {
+        case 1:
+            printf("********* User Info ********\n");
+            show_user_info(login_user_index);
+            user_dashboard(login_user_index);
+            break;
+        case 2:
+            printf("********* Edit Your Info ******\n");
+            edit_user_info(login_user_index);
+            break;
+        case 3:
+            printf("********* Transfer Your Point *******\n");
+            break;
+        case 4:
+            printf("********** Bye Bye **********\n");
+            menu();
+            break;
+        default:
+            printf("******** Wrong Choice! *********\n");
+            user_dashboard(login_user_index);
+    }
 }
 
 void admin_dashboard(int login_user_index) {
+    printf("\n Welcome Admin (%s) \n", users[login_user_index].name);
+    int option = 0;
+    printf("Enter 1 To See Your Info. Enter 2 To Edit Your Info.\nEnter 3 To Manage Users. Enter 4 To Logout\n");
+    printf("Enter Here => ");
+    scanf("%d", &option);
 
+    switch (option) {
+        case 1:
+            printf("********* User Info ********\n");
+            show_user_info(login_user_index);
+            admin_dashboard(login_user_index);
+            break;
+        case 2:
+            printf("********* Edit Your Info ******\n");
+            edit_user_info(login_user_index);
+            break;
+        case 3:
+            printf("********* Manage Users *******\n");
+            break;
+        case 4:
+            printf("********** Bye Bye **********\n");
+            menu();
+            break;
+        default:
+            printf("******** Wrong Choice! *********\n");
+            admin_dashboard(login_user_index);
+    }
 }
 
+void show_user_info(int login_user_index) {
+    printf("Your Name is     %s.\n", users[login_user_index].name);
+    printf("Your Email is    %s.\n", users[login_user_index].email);
+    printf("Your Point is    %d.\n ", users[login_user_index].point);
+    printf("Your Role is     %s.\n ", users[login_user_index].role);
+    printf("Your Phone is    %d.\n ", users[login_user_index].phone);
+    printf("Your Postcode is %d.\n ", users[login_user_index].postcode);
+    printf("Your address is  %s.\n ", users[login_user_index].address);
+}
+
+void edit_user_info(int login_user_index) {
+    int e_option = 0;
+    int is_valid_email = 0;
+    int is_valid_phone = 0;
+    int is_valid_password = 0;
+    char e_password[20];
+    char e_name[20];
+    char e_email[20];
+    int e_phone;
+    int e_postcode;
+    char e_address[50];
+
+    printf(
+        "Enter 1 To Edit Name\nEnter 2 To Email\nEnter 3 To Edit Phone\nEnter 4 To Edit Postcode\nEnter 5 To Edit Address\nEnter 6 To Change Password => ");
+    scanf("%d", &e_option);
+
+    if (e_option == 1) {
+        printf("Enter User Name => ");
+        scanf(" %[^\n]", &e_name[0]);
+        copy_two_char_array(users[login_user_index].name, e_name);
+        printf("\n Updated User Name Successfully and New User Name is (%s).\n", users[login_user_index].name);
+    } else if (e_option == 2) {
+        while (!is_valid_email) {
+            printf("Enter User Email => ");
+            scanf(" %[^\n]", &e_email[0]);
+            is_valid_email = validate_email(e_email);
+        }
+        copy_two_char_array(users[login_user_index].email, e_email);
+        printf("\n Updated User Email Successfully and New User Email is (%s).\n", users[login_user_index].email);
+    } else if (e_option == 3) {
+        while (!is_valid_phone) {
+            printf("Enter User Phone => ");
+            scanf("%d", &e_phone);
+            is_valid_phone = validate_phone(e_phone);
+        }
+        users[login_user_index].phone = e_phone;
+        printf("\n Updated User Phone Successfully and New User Phone is (%d).\n", users[login_user_index].phone);
+    } else if (e_option == 4) {
+        printf("Enter User Postcode => ");
+        scanf("%d", &e_postcode);
+        users[login_user_index].postcode = e_postcode;
+        printf("\n Updated User Postcode Successfully and New User Postcode is (%d).\n",
+               users[login_user_index].postcode);
+    } else if (e_option == 5) {
+        printf("Enter User Address => ");
+        scanf(" %[^\n]", &e_address[0]);
+
+        copy_two_char_array(users[login_user_index].address, e_address);
+        printf("\n Updated User Address Successfully and New User Address is (%s).\n", users[login_user_index].address);
+    } else if (e_option == 6) {
+        if (!confirm_password(login_user_index)) {
+            int option = 0;
+            printf("Password Confirmation is invalid!\nEnter 1 To Retry\nEnter Other To Stop\n");
+            printf("Enter Here => ");
+            scanf("%d", &option);
+            if (option == 1) {
+                edit_user_info(login_user_index);
+            } else {
+                show_dashboard();
+            }
+        }
+        while (!is_valid_password) {
+            printf(
+                "Password Must Include at least One Small Letter\nOne Capital Letter\nOne Special Character and\nMust Be at least 6 Characters Long\nEnter User Password => ");
+            scanf(" %[^\n]", &e_password[0]);
+            is_valid_password = validate_password(e_password);
+        }
+        copy_two_char_array(users[login_user_index].password, e_password);
+        printf("\n Updated User Password Successfully and New Password is (%s).\n", users[login_user_index].password);
+    } else {
+        printf("\n***** Wrong Option ******\n");
+        edit_user_info(login_user_index);
+    }
+
+    printf("\nEnter 1 to continue or 2 to go back=> ");
+    scanf("%d", &e_option);
+    if (e_option == 1) {
+        edit_user_info(login_user_index);
+    } else {
+        show_dashboard();
+    }
+}
+
+int confirm_password(int login_user_index) {
+    char c_password[20];
+    printf("\n Confirm Your Password => ");
+    scanf(" %[^\n]", &c_password[0]);
+    if (check_two_char_array(users[login_user_index].password, c_password)) {
+        return 1;
+    }
+    return 0;
+}
 
 /**
  * Functions End ...............................
@@ -411,7 +583,7 @@ void admin_dashboard(int login_user_index) {
 
 int check_phone_duplication(int phone) {
     for (int x = 0; x < g_user_count; x++) {
-        if(users[x].phone == phone) {
+        if (users[x].phone == phone) {
             return 1;
         }
     }
@@ -420,8 +592,8 @@ int check_phone_duplication(int phone) {
 
 int check_email_duplication(char email[50]) {
     for (int x = 0; x < g_user_count; x++) {
-        int is_same = check_two_char_array(users[x].email,email);
-        if(is_same) {
+        int is_same = check_two_char_array(users[x].email, email);
+        if (is_same) {
             return 1;
         }
     }
@@ -431,7 +603,7 @@ int check_email_duplication(char email[50]) {
 
 void copy_two_char_array(char target[50], char data[50]) {
     for (int x = 0; x < 50; x++) {
-        if (data[x] == '\0') {
+        if (target[x] == '\0') {
             break;
         }
         target[x] = data[x];
@@ -439,7 +611,6 @@ void copy_two_char_array(char target[50], char data[50]) {
 }
 
 int check_two_char_array(char target[50], char data[50]) {
-
     int targetCount = get_char_array_count(target);
     int dataCount = get_char_array_count(data);
 
@@ -490,7 +661,8 @@ int is_capital_letter(char data) {
 }
 
 int is_special_character(char data) {
-    if ((data >= 32 && data <= 47) || (data >= 58 && data <= 64) || (data >= 91 && data <= 96) || (data >= 123 && data <= 126)) {
+    if ((data >= 32 && data <= 47) || (data >= 58 && data <= 64) || (data >= 91 && data <= 96) || (
+            data >= 123 && data <= 126)) {
         return 1;
     }
     return 0;
